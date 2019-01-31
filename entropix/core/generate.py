350e35d08954d4e29aef7eb54ed609306ded6218
Ludovica Pannitto <ludovica@rov-cimec-18112.unitn.it>
2019-01-31 18:19:02 +0100

    added count-based model

--- entropix/core/generate.py
+++ entropix/core/generate.py
@@ -1,128 +1,113 @@
 """A basic count-based model using sparse matrices and no ppmi."""
 import os
-import collections
-from datetime import datetime
+import logging
 from scipy import sparse
 
-def count_cooc (fname, window, freqdist, fr_threshold, word_to_idx_map):
-    print(fr_threshold)
+from tqdm import tqdm
+import entropix.core.count as count
 
-	final_rows = []
-	final_columns = []
-	final_data = []
-    with open(fname, 'r', encoding='utf-8') as fin:
-        for line_num, line in enumerate(fin):
-			linesplit = line.strip().split()
-			for idx, tok in enumerate(linesplit):
-					tok_mapped_index = word_to_idx_map[tok]
-					start = 0 if window == 0 else max(0, idx-window)
-					end = idx
-					while start<end:
-						cooctok = linesplit[start]
-						cooctok_mapped_index = word_to_idx_map[cooctok]
-						final_rows.append(tok_mapped_index)
-						final_columns.append(cooctok_mapped_index)
-						final_rows.append(cooctok_mapped_index)
-						final_columns.append(tok_mapped_index)
-						start+=1
-			if not line_num%100000:
-				print(line_num)
+logger = logging.getLogger(__name__)
+
+__all__ = ('generate_model')
 
 
-	idx_to_word_map = {k:w for w,k in word_to_idx_map.items() if freqdist[w]>fr_threshold}
+def _get_output_filenames(output_dirpath, corpus_filepath, min_count, win_size):
+    if corpus_filepath.endswith('.txt'):
+        output_filepath_matrix = os.path.join(
+            output_dirpath, '{}.mincount-{}.win-{}'.format(
+                os.path.basename(corpus_filepath).split('.txt')[0], min_count,
+                win_size))
+
+        output_filepath_map = os.path.join(
+            output_dirpath, '{}.mincount-{}.win-{}.map'.format(
+                os.path.basename(corpus_filepath).split('.txt')[0], min_count,
+                win_size))
+    else:
+        output_filepath_matrix = os.path.join(
+            output_dirpath,
+            '{}.mincount-{}.win-{}'.format(os.path.basename(corpus_filepath),
+                                           min_count, win_size))
+        output_filepath_map = os.path.join(
+            output_dirpath,
+            '{}.mincount-{}.win-{}.map'.format(os.path.basename(corpus_filepath),
+                                               min_count, win_size))
+    return output_filepath_matrix, output_filepath_map
 
 
-	print("reverse freqdist len")
-	print(len(idx_to_word_map))
+def generate_model(output_dirpath, corpus_filepath, min_count=0, win_size=2):
+    """Generate ..."""
+    output_filepath_matrix, output_filepath_map =\
+        _get_output_filenames(output_dirpath, corpus_filepath, min_count,
+                              win_size)
 
-	print("freqdist len")
-	print(len(freqdist))
+    word_to_count_dic = count.count_words(output_dirpath, corpus_filepath)
+    word_to_idx_dic = {}
+    i = 0
+    for word, frequency in word_to_count_dic.items():
+        if frequency >= min_count:
+            word_to_idx_dic[word] = i
+            i += 1
+
+    # word_to_idx_dic = {w: i for w, i in zip(word_to_count_dic.keys(),
+    #                                         range(len(word_to_count_dic)))
+    #                    if word_to_count_dic[w] >= min_count}
+
+    logger.info('Filtering out vocabulary words with frequency lower than {}, '
+                'shrinking size by {:.2f}% from {} to {}.'
+                .format(min_count,
+                        100-len(word_to_idx_dic)*100.0/len(word_to_count_dic),
+                        len(word_to_count_dic), len(word_to_idx_dic)))
+
+    rows = []
+    columns = []
+
+    with open(corpus_filepath, 'r', encoding='utf-8') as input_stream:
+        for line_n, line in tqdm(enumerate(input_stream)):
+            linesplit = line.strip().split()
+            for token_pos, token in enumerate(linesplit):
+
+                start = 0 if win_size == 0 else max(0, token_pos-win_size)
+                while start < token_pos:
+                    ctx = linesplit[start]
+                    if token in word_to_idx_dic and ctx in word_to_idx_dic:
+                        token_idx = word_to_idx_dic[token]
+                        ctx_idx = word_to_idx_dic[ctx]
+
+                        rows.append(token_idx)
+                        columns.append(ctx_idx)
+                        rows.append(ctx_idx)
+                        columns.append(token_idx)
+                    start += 1
+
+    logger.info('Building CSR sparse matrix')
+    M = sparse.csr_matrix(([1]*len(rows), (rows, columns)),
+                          shape=(len(word_to_idx_dic), len(word_to_idx_dic)),
+                          dtype='f')
+    logger.info('Matrix info: {} non-zero entres, {} shape, {:.6f} density'
+                 .format(M.getnnz(), M.shape, M.getnnz()*1.0/(M.shape[0]*M.shape[1])))
+    logger.info('Saving matrix to {}.npz'.format(output_filepath_matrix))
+    sparse.save_npz(output_filepath_matrix, M)
+
+    with open(output_filepath_map, 'w', encoding='utf-8') as output_stream:
+        for word, idx in word_to_idx_dic.items():
+            print('{}\t{}'.format(idx, word), file=output_stream)
+    return M, word_to_idx_dic
+    #filter entries based on min_count
+#    filtered_idx_to_word_dic={k:w for w,k in word_to_idx_dic.items()
+#                              if word_to_counts_dic[w]>min_count} ## > or >=?
+
+#    shifted_map={k:inc for k, inc in zip(accepted_idx_to_word_map.keys(),
+#                 range(len(accepted_idx_to_word_map)))}
+
+#    keep_indexes = []
+#    i = 0
+#    for row, column in zip(final_rows, final_columns):
+#        if row in accepted_idx_to_word_map and column in accepted_idx_to_word_map:
+#            keep_indexes.append(i)
+#        i += 1
 
 
-	new_map = {k:inc for k, inc in zip(idx_to_word_map.keys(), range(len(idx_to_word_map)))}
-
-
-	keep_indexes = []
-	idx = 0
-	for row, column in zip(final_rows, final_columns):
-		if row in idx_to_word_map and column in idx_to_word_map:
-			keep_indexes.append(idx)
-
-
-		idx+=1
-
-	return [1]*len(keep_indexes), [new_map[final_rows[i]] for i in keep_indexes], [new_map[final_columns[i]] for i in keep_indexes], new_map
-
-
-def compute_word_freqs (fname):
-
-	voc = collections.defaultdict(int)
-	word_ind = {}
-	latest_ind = 0
-
-	with open(fname) as fin:
-
-		for line in fin:
-			linesplit = line.strip().split()
-
-			for tok in linesplit:
-				voc[tok]+=1
-				if not tok in word_ind:
-					word_ind[tok]=latest_ind
-					latest_ind+=1
-
-	return voc, word_ind
-
-if __name__ == "__main__":
-
-	_NTHR = 1
-	_WINSIZE = 2
-	_MINCOUNT = 50
-
-	#~ fname = "enwiki.20190120.txt.sample2.0.head"
-	fname = "enwiki.20190120.txt.sample2.0"
-
-	global_word_freqs, global_word_to_idx = compute_word_freqs(fname)
-
-	print(len(global_word_freqs))
-
-	b = os.path.getsize(fname)
-
-	B_size = b//_NTHR+1
-
-	#~ parameters = [{"fname" : fname, "idx_to_seek" : x, "blocksize" : B_size, "window": _WINSIZE, "freqdist": global_word_freqs, "fr_threshold": _MINCOUNT, "word_to_idx_map": global_word_to_idx} for x in range(0, b, B_size)]
-	parameters = [{"fname" : fname, "window": _WINSIZE, "freqdist": global_word_freqs, "fr_threshold": _MINCOUNT, "word_to_idx_map": global_word_to_idx} for x in range(0, b, B_size)]
-
-
-	#~ p = multiprocessing.Pool(_NTHR)
-
-	print(datetime.now())
-	print("before mapping")
-
-	#~ res = [p.apply_async(count_cooc, kwds=pardict) for pardict in parameters]
-	#~ out = [r.get() for r in res]
-
-
-	data, rows, columns, new_map = count_cooc(**parameters[0])
-	n_dim = len(new_map)
-	mat = sparse.csr_matrix((data, (rows, columns)), shape=(n_dim, n_dim), dtype='f')
-
-	idx_to_word_map = {}
-
-	for w, k in global_word_to_idx.items():
-		if k in new_map:
-			idx_to_word_map[k] = w
-
-	print("SHAPE")
-	print(mat.shape)
-	print("NONZERO")
-	print(mat.getnnz())
-
-	sparse.save_npz("{}.mincount-{}".format(fname,_MINCOUNT), mat)
-
-	with open("{}.mincount-{}.map".format(fname, _MINCOUNT), "w") as fout:
-		for k, w in idx_to_word_map.items():
-			fout.write("{}\t{}\n".format(k, w))
-
-	print(datetime.now())
-	print("done dumping")
+#    final_data = [1]*len(keep_indexes)
+#    final_rows = [shifted_map[final_rows[i]] for i in keep_indexes]
+#    final_columns = [shifted_map[final_columns[i]] for i in keep_indexes]
+#    n_dim = len(shifted_map)
