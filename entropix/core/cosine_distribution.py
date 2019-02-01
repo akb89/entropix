35d5577aa48f164fa0681c32292aa88daa0e9d6f
Ludovica Pannitto <ludovica@rov-cimec-18112.unitn.it>
2019-02-01 17:43:59 +0100

    paiwise cosine computation

---
+++ entropix/core/cosine_distribution.py
@@ -0,0 +1,114 @@
+"""Compute paiwise cosine similarity between vocabulary items
+and their distribution."""
+import os
+import logging
+import math
+import multiprocessing
+import gzip
+import functools
+from scipy import sparse
+from scipy import spatial
+
+from tqdm import tqdm
+
+
+logger = logging.getLogger(__name__)
+
+__all__ = ('cosine_distribution')
+
+
+def _load_vocabulary(vocabulary_filepath):
+    voc = set()
+
+    with open(vocabulary_filepath, encoding='utf-8') as input_stream:
+        for line in input_stream:
+            voc.add(line.strip().lower())
+    return voc
+
+
+def _load_space(space_filepath):
+    basename_space_filepath = os.path.basename(space_filepath)
+    map_filepath = '{}.map'.format(basename_space_filepath)
+    if basename_space_filepath.endswith('.npz'):
+        map_filepath = '{}.map'.format(basename_space_filepath[:-len('.npz')])
+
+    if not os.path.exists(os.path.join(os.path.dirname(space_filepath), map_filepath)):
+        logger.error('There is no .map file in the space folder')
+        raise IOError
+
+    try:
+        M = sparse.load_npz(space_filepath)
+    except IOError as err:
+        logger.error('The space file cannot be loaded. '
+                     'Please specify a .npz file.'
+                     'Error {} .'.format(err))
+
+    try:
+        idx_to_word_dic = {}
+        with open(os.path.join(os.path.dirname(space_filepath), map_filepath),
+                  encoding='utf-8') as input_stream:
+            for line in input_stream:
+                linesplit = line.strip().split()
+                idx_to_word_dic[int(linesplit[0])] = linesplit[1]
+    except Exception as err:
+        logger.error('Impossible to load map file.'
+                     'Error {}'.format(err))
+
+    return M, idx_to_word_dic
+
+
+def _process(M, idx_to_word_dic, idx):
+    cosines_dic = {}
+    vector = M.getrow(idx).toarray()
+    for idx2 in idx_to_word_dic:
+        if idx2 > idx:
+            vector2 = M.getrow(idx2).toarray()
+            cosine = 1-spatial.distance.cosine(vector, vector2)
+            if not math.isnan(cosine):
+                cosines_dic[(idx, idx2)] = cosine
+            else:
+                logger.info('Undefined cosine similarity for pair '
+                            '{} {}'.format(idx_to_word_dic[idx],
+                                           idx_to_word_dic[idx2]))
+    return cosines_dic, idx
+
+
+def cosine_distribution(output_dirpath, space_filepath, threads_number, bin_size,
+                        vocabulary_filepath=''):
+    """Compute paiwise cosine similarity between vocabulary items and their
+    distribution."""
+    M, idx_to_word_dic = _load_space(space_filepath)
+
+    vocabulary = set()
+    if not len(vocabulary_filepath) == 0:
+        vocabulary = _load_vocabulary(vocabulary_filepath)
+
+    if vocabulary:
+        idx_to_word_dic = {k: w for k, w in idx_to_word_dic.items()
+                           if w in vocabulary}
+        logger.info('{} out of {} vocabulary items found.'.format(len(idx_to_word_dic),
+                                                                  len(vocabulary)))
+    number_of_bins = 1/bin_size
+    freqdist = [0]*int(number_of_bins)
+    cosinepairs_filepath = os.path.join(output_dirpath,
+                                        '{}.cos.gz'.format(os.path.basename(space_filepath)))
+    with gzip.open(cosinepairs_filepath, 'wt', encoding='utf-8') as output_stream:
+        with multiprocessing.Pool(threads_number) as pool:
+            process = functools.partial(_process, M, idx_to_word_dic)
+            for partial_cosine_dic, idx in tqdm(pool.imap_unordered(process,
+                                                idx_to_word_dic.keys())):
+                for index_pair, cosine in partial_cosine_dic.items():
+                    idx, idx2 = index_pair
+                    freqdist[int(cosine/bin_size)] += 1
+                    print('{}\t{}\t{}'.format(idx_to_word_dic[idx],
+                                              idx_to_word_dic[idx2], cosine),
+                           file=output_stream)
+
+    distribution_filepath = os.path.join(output_dirpath, 'cosine_distribution.txt')
+    with open(distribution_filepath, 'w', encoding='utf-8') as output_stream:
+        print('range\tN', file=output_stream)
+        for i, k in enumerate(freqdist):
+            print('{}<=x<{}\t{}'.format(round(i*bin_size, 5),
+                                        round((i+1)*bin_size, 5), k),
+                  file=output_stream)
+    return freqdist
--- entropix/main.py
+++ entropix/main.py
@@ -12,6 +12,7 @@ import entropix.utils.config as cutils
 import entropix.core.count as count
 import entropix.core.compute as compute
 import entropix.core.generate as generate
+import entropix.core.cosine_distribution as cosine_distribution
 
 logging.config.dictConfig(
     cutils.load(
@@ -57,6 +58,25 @@ def _generate(args):
     generate.generate_model(output_dirpath, args.corpus, args.threshold)
 
 
+def _compute_pairwise_cosines(args):
+    if not args.output:
+        output_dirpath = os.path.dirname(args.space)
+    else:
+        output_dirpath = args.output
+    if not os.path.exists(output_dirpath):
+        logger.info('Creating directory {}'.format(output_dirpath))
+        os.makedirs(output_dirpath)
+    else:
+        logger.info('Saving to directory {}'.format(output_dirpath))
+    if not args.vocabulary:
+        vocabulary = ''
+    else:
+        vocabulary = args.vocabulary
+    cosine_distribution.cosine_distribution(output_dirpath, args.space,
+                                            args.threads_number, args.bin_size,
+                                            vocabulary)
+
+
 def main():
     """Launch entropix."""
     parser = argparse.ArgumentParser(prog='entropix')
@@ -91,5 +111,22 @@ def main():
                                  help='frequency threshold on vocabulary')
     parser_generate.add_argument('-w', '--winsize', default=2, type=int,
                                  help='size of context window')
+    parser_cosine = subparsers.add_parser(
+        '_compute_pairwise_cosines', formatter_class=argparse.RawTextHelpFormatter,
+        help='generate pairiwise cosine similarity between vocabulary items')
+    parser_cosine.set_defaults(func=_compute_pairwise_cosines)
+    parser_cosine.add_argument('-v', '--vocabulary',
+                               help='set of vocabulary items to compute '
+                               'the distribution for.'
+                               'If not set, will default to whole vocabulary')
+    parser_cosine.add_argument('-s', '--space', required=True,
+                               help='distributional space')
+    parser_cosine.add_argument('-o', '--output',
+                               help='absolute path to output directory. '
+                               'If not set, will default to space dir')
+    parser_cosine.add_argument('-t', '--threads_number', default=1, type=int,
+                               help='number of threads')
+    parser_cosine.add_argument('-b', '--bin_size', default=0.1, type=float,
+                               help='bin size for the distribution output')
     args = parser.parse_args()
     args.func(args)
