a6e616df871021da4133284465f69218cfbab18e
AKB <akb@3azouz.net>
2019-02-05 09:37:51 +0100

    Updated XP009

--- experiments/xp009.py
+++ experiments/xp009.py
@@ -8,7 +8,7 @@ import entropix.utils.files as futils
 if __name__ == '__main__':
     print('Running entropix XP#009')
 
-    WIKI_DIRPATH = '/home/kabbach/witokit/data/wiki/'
+    WIKI_DIRPATH = '/home/kabbach/witokit/data/wiki/small/'
     MODEL_DIRPATH = '/home/kabbach/entropix/models/'
     MIN_COUNT = 300
     WIN_SIZE = 5
