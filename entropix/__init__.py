1a0733fe956f96ab869e6269e2ca2d69ce4ba20c
Ludovica Pannitto <ludovica@rov-cimec-18112.unitn.it>
2019-02-06 11:49:24 +0100

    misc. fixes

--- entropix/__init__.py
+++ entropix/__init__.py
@@ -6,9 +6,9 @@ from .core.evaluator import evaluate_distributional_space as evaluate
 from .core.generator import generate_distributional_model as generate
 from .core.reducer import reduce_matrix_via_svd as reduce
 from .utils.files import get_sparsematrix_filepath as get_model_filepath
-from .utils.files import get_sing_vectors_filepath as get_sing_vectors_filepath
-from .utils.files import get_sing_values_filepath as get_sing_values_filepath
+from .utils.files import get_singvectors_filepath as get_singvectors_filepath
+from .utils.files import get_singvalues_filepath as get_singvalues_filepath
 
 __all__ = ('count', 'compute', 'evaluate', 'generate', 'reduce',
-           'get_model_filepath', 'get_sing_vectors_filepath',
-           'get_sing_values_filepath')
+           'get_model_filepath', 'get_singvectors_filepath',
+           'get_singvalues_filepath')
