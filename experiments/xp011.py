3ea4adb766caf7eb826ac5983e02cef028ef9596
Ludovica Pannitto <ludovica@rov-cimec-18112.unitn.it>
2019-02-08 10:39:53 +0100

    refactoring

--- experiments/xp011.py
+++ experiments/xp011.py
@@ -1,5 +1,6 @@
 """Generate cosine similarities distribution for a model."""
 
+import os
 import entropix
 
 if __name__ == '__main__':
@@ -7,8 +8,13 @@ if __name__ == '__main__':
 
     _OUTPUT_DIRPATH = '/home/ludovica/Desktop/debug_out/'
     _MODEL_FILEPATH = '/home/ludovica/Desktop/debug_out/itwiki.20190120.mincount-100.win-2.npz'
+    _VOCAB_FILEPATH = '/home/ludovica/Desktop/debug_out/itwiki.20190120.mincount-100.win-2.vocab'
     _NUM_THREADS = 4
     _BIN_SIZE = 0.1
 
+    assert os.path.exists(_OUTPUT_DIRPATH)
+    assert os.path.exists(_MODEL_FILEPATH)
+    assert os.path.exists(_VOCAB_FILEPATH)
+
     entropix.pwcosine_distribution(_OUTPUT_DIRPATH, _MODEL_FILEPATH,
-                                   _NUM_THREADS, _BIN_SIZE)
+                                   _VOCAB_FILEPATH, _NUM_THREADS, _BIN_SIZE)
