6d51a959fc8a0a53f9fdb79820b4cb3bd2a78b55
Ludovica Pannitto <ludovica@rov-cimec-18112.unitn.it>
2019-04-02 16:46:16 +0200

    misc updates visualization

--- experiments/xpviz001.py
+++ experiments/xpviz001.py
@@ -27,12 +27,12 @@ if __name__ == '__main__':
 #        print(sing_vectors_filepath)
 #        sing_vectors_filepath = os.path.basename(sing_vectors_filepath)
 #        sing_vectors_filepath = os.path.join(MODELS_DIRPATH, sing_vectors_filepath)
-    extractor.extract_top_participants(
-            OUTPUT_DIRPATH, '/home/ludovica/entropix_models/enwiki.20190120.mincount-300.win-2.ppmi.k10000.singvectors.npy', vocab_filepath, 50, True)
+#    extractor.extract_top_participants(
+#            OUTPUT_DIRPATH, '/home/ludovica/entropix_models/enwiki.20190120.mincount-300.win-2.ppmi.k10000.singvectors.npy', vocab_filepath, 50, True)
 
     # Issue 2:
     # General overlap between features
 
     print('Issue2')
-    input_filepath = 'enwiki.20190120.mincount-300.win-2.ppmi.k10000.top-20.abs.CM'
+    input_filepath = 'enwiki.20190120.mincount-300.win-2.ppmi.k10000.top-50.abs.CM'
     visualizer.visualize_heatmap(OUTPUT_DIRPATH, OUTPUT_DIRPATH+input_filepath, None)
