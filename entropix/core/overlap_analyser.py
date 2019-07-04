4c8f42d2e11ee99d846413c78f81d65cc9122198
Ludovica Pannitto <ludovica@rov-cimec-18112.unitn.it>
2019-07-04 17:26:28 +0200

    minor changes

--- entropix/core/overlap_analyser.py
+++ entropix/core/overlap_analyser.py
@@ -72,10 +72,13 @@ def analyse_overlap(model_filepath, vocab_filepath, dataset_name):
 #    print(submatrix_right)
     euclidean = np.diag(euclidean_distances(submatrix_left, submatrix_right))
     cosines = np.diag(cosine_similarity(submatrix_left, submatrix_right))
-    print(euclidean)
 #    euclidean = mutils.similarity(submatrix_left, submatrix_right, 'euclidean')
 
     row_wise_euclidean = []
+    percentiles = {10:[], 20:[], 30:[], 40:[], 50:[], 60:[], 70:[], 80:[], 90:[]}
+    positions = {}
+    for i in range(50):
+        positions[i] = []
 
     i = 0
     for left_w, right_w in pairs_dataset:
@@ -86,13 +89,26 @@ def analyse_overlap(model_filepath, vocab_filepath, dataset_name):
         x_inters_y = x_row.minimum(y_row)
         nonzero_indices = x_inters_y.nonzero()[1]
 
- #        row_wise_euclidean.append(euclidean(x_row[nonzero_indices], y_row[nonzero_indices]))
+        x_reduced = x_row[0,nonzero_indices].todense()
+        y_reduced = y_row[0,nonzero_indices].todense()
+#        print(x_reduced)
+#        print(y_reduced)
+        row_wise_euclidean.append(np.linalg.norm(x_reduced - y_reduced))
+
+#        print(row_wise_euclidean)
+#        input()
         nonzero_values = [x_inters_y[0,i] for i in nonzero_indices]
         nonzero_indices = [reverse_vocab[el] for el in nonzero_indices]
 
         number_of_shared_dims[left_w][right_w] = len(nonzero_values)
 
         sorted_value_idx_pair = sorted(zip(nonzero_values,nonzero_indices), reverse = True)
+        sorted_values = [x[0] for x in sorted_value_idx_pair]
+
+        for p in range(10, 100, 10):
+            percentiles[p].append(np.percentile(sorted_values, p))
+        for p in range(50):
+            positions[p].append(sorted_values[min(len(sorted_values)-1, p)])
 
         top_shared_dims[left_w][right_w] = [el[1] for el in sorted_value_idx_pair[:10]]
         bottom_shared_dims[left_w][right_w] = [el[1] for el in sorted_value_idx_pair[-10:]]
@@ -126,5 +142,12 @@ def analyse_overlap(model_filepath, vocab_filepath, dataset_name):
     print ('Median PPMI', stats.spearmanr(median_ppmi, dataset_sim)[0])
     print ('avg_ppmi', stats.spearmanr(avg_ppmi, dataset_sim)[0])
     print ('euclidean distance', stats.spearmanr(euclidean, dataset_sim)[0])
-#    print ('intersection euclidean distance', stats.spearmanr(row_wise_euclidean, dataset_sim)[0])
+    print ('intersection euclidean distance', stats.spearmanr(row_wise_euclidean, dataset_sim)[0])
     print ('cosine similarity', stats.spearmanr(cosines, dataset_sim)[0])
+
+
+    for p in range(10, 100, 10):
+        print ('value at percentile', p, stats.spearmanr(percentiles[p], dataset_sim)[0])
+
+    for p in range(50):
+        print ('value at position', p, stats.spearmanr(positions[p], dataset_sim)[0])
