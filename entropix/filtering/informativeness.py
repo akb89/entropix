f27f3e9ca39f8dd422271349772724e29c57264e
AKB <akb@3azouz.net>
2019-09-23 10:03:56 +0200

    test

--- entropix/filtering/informativeness.py
+++ entropix/filtering/informativeness.py
@@ -32,13 +32,13 @@ class Informativeness():
     #@lru_cache(maxsize=5)
     def _get_prob_distribution(self, context):
         word2_indices = [self._model.wv.vocab[w].index for w in context if w in self._model.wv.vocab]
-        l1 = np.sum(self._model.wv.vectors[word2_indices], axis=0)
-        if word2_indices and self._model.cbow_mean:
-            l1 /= len(word2_indices)
-        # propagate hidden -> output and take softmax to get probabilities
-        prob_values = np.exp(np.dot(l1, self._model.trainables.syn1neg.T))
-        prob_values /= sum(prob_values)
-        # prob_values = np.random.dirichlet(np.ones(151166), size=1)[0]
+        # l1 = np.sum(self._model.wv.vectors[word2_indices], axis=0)
+        # if word2_indices and self._model.cbow_mean:
+        #     l1 /= len(word2_indices)
+        # # propagate hidden -> output and take softmax to get probabilities
+        # prob_values = np.exp(np.dot(l1, self._model.trainables.syn1neg.T))
+        # prob_values /= sum(prob_values)
+        prob_values = np.random.dirichlet(np.ones(151166), size=1)[0]
         return prob_values
 
     #@lru_cache(maxsize=5)
