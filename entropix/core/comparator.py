9539effb03f79fb90aecf0f244cfe4451226f211
AKB <akb@3azouz.net>
2019-10-07 16:39:05 +0200

    fixed ram problem

--- entropix/core/comparator.py
+++ entropix/core/comparator.py
@@ -3,32 +3,59 @@ import logging
 
 import numpy as np
 import scipy.spatial as spatial
+import entropix.utils.metrix as metrix
 
 logger = logging.getLogger(__name__)
 
 
+def _get_n_nearest_neighbors(idx, model, n):
+    vector = model[idx]
+    sim = []
+    for i in range(model.shape[0]):
+        if i == idx:
+            sim.append(0)
+        else:
+            ivector = model[i]
+            cos = metrix.cosine_similarity(vector, ivector)
+            sim.append(cos)
+    return set(np.argsort(sim)[::-1][:n])
+
+
+def _compare_low_ram(model1, model2, n):
+    variance = []
+    assert model1.shape[0] == model2.shape[0]
+    for idx in range(model1.shape[0]):
+        neighb1 = _get_n_nearest_neighbors(idx, model1, n)
+        neighb2 = _get_n_nearest_neighbors(idx, model2, n)
+        variance.append(1 - len(neighb1.intersection(neighb2))/n)
+    return variance
+
+
 def _compare(model1, model2, n):
-    # compute cosine similarities
-    logger.info('Computing cosine similarities...')
-    cos1 = 1 - spatial.distance.cdist(model1, model1, 'cosine')
-    cos2 = 1 - spatial.distance.cdist(model2, model2, 'cosine')
-    # postprocess to avoid taking the word itself as its own nearest neighbor
-    cos1 = cos1 - np.diag(np.ones(model1.shape[0]))
-    cos2 = cos2 - np.diag(np.ones(model2.shape[0]))
-    # compute the n-nearest neighbor indices
-    logger.info('Computing nearest neighbor indices...')
-    idx1 = np.argpartition(cos1, -n, axis=1)[:, -n:]
-    idx2 = np.argpartition(cos2, -n, axis=1)[:, -n:]
-    # sort to be able to use np.count_nonzero(A==B, axis=1)
-    idx1.sort()
-    idx2.sort()
-    logger.info('Computing variance...')
-    # compute the variance following Pierrejean and Tanguy 2018
-    return 1 - np.count_nonzero(idx1 == idx2, axis=1) / n
+    try:
+        # compute cosine similarities
+        logger.info('Computing cosine similarities...')
+        cos1 = 1 - spatial.distance.cdist(model1, model1, 'cosine')
+        cos2 = 1 - spatial.distance.cdist(model2, model2, 'cosine')
+        # postprocess to avoid taking the word itself as its own nearest neighbor
+        cos1 = cos1 - np.diag(np.ones(model1.shape[0]))
+        cos2 = cos2 - np.diag(np.ones(model2.shape[0]))
+        # compute the n-nearest neighbor indices
+        logger.info('Computing nearest neighbor indices...')
+        idx1 = np.argpartition(cos1, -n, axis=1)[:, -n:]
+        idx2 = np.argpartition(cos2, -n, axis=1)[:, -n:]
+        # sort to be able to use np.count_nonzero(A==B, axis=1)
+        idx1.sort()
+        idx2.sort()
+        logger.info('Computing variance...')
+        # compute the variance following Pierrejean and Tanguy 2018
+        return 1 - np.count_nonzero(idx1 == idx2, axis=1) / n
+    except MemoryError:
+        return _compare_low_ram(model1, model2, n)
 
 
 def _align_model_vocab(model1, model2, vocab1, vocab2):
-    # align model1 with model2's vocab
+    logger.info('Aligning model vocabularies...')
     vocab_2_to_vocab1 = {idx: vocab1[word] for word, idx in vocab2.items()
                          if word in vocab1}
     _model1 = np.empty(shape=(len(vocab_2_to_vocab1), model1.shape[1]))
@@ -48,6 +75,7 @@ def align_vocab(model1, model2, vocab1, vocab2):
             return _align_model_vocab(model1, model2, vocab1, vocab2)
         if vocab2[word] != idx:
             return _align_model_vocab(model1, model2, vocab1, vocab2)
+    logger.info('Processing already aligned vocabularies')
     return model1, model2
 
 
---
+++ tests/test_comparator.py
@@ -0,0 +1,36 @@
+"""Test the comparator."""
+import numpy as np
+
+import entropix.core.comparator as comparator
+
+
+def test_compare():
+    A = np.array([[0,1,2,3], [0,1,2,3], [0,1,2,3], [0,1,2,3]])
+    B = np.array([[0,1,2,3], [0,1,2,3], [0,1,2,3], [0,1,2,3]])
+    variance = comparator.compare(A, B, {}, {}, n=2)
+
+
+def test_align_vocab():
+    pass
+
+
+def test_get_n_nearest_neighbors():
+    A = np.array([[0, 1], [1, 0], [0, 1], [1, 1], [2, 2]])
+    n1 = comparator._get_n_nearest_neighbors(0, A, 2)
+    assert n1 == {2, 4}
+    n2 = comparator._get_n_nearest_neighbors(2, A, 2)
+    assert n2 == {0, 4}
+    n3 = comparator._get_n_nearest_neighbors(1, A, 2)
+    assert n3 == {3, 4}
+
+
+def test_compare_low_ram():
+    A = np.array([[0, 1], [1, 0], [0, 1], [1, 1], [2, 2]])
+    var = comparator._compare_low_ram(A, A, 2)
+    assert var == [0, 0, 0, 0, 0]
+    B = np.array([[1, 0], [0, 1], [1, 0], [1, 1], [2, 2]])
+    var = comparator._compare_low_ram(A, B, 2)
+    assert var == [0, 0, 0, 0, 0]
+    C = np.array([[0, 0], [0, 1], [1, 0], [1, 1], [2, 2]])
+    var = comparator._compare_low_ram(B, C, 2)
+    assert var == [0.5, 0.5, 0, 0.5, 0.5]
