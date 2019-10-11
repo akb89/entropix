36afbca1a52c8244c1bd9c4cbb219d7fbeb5c661
AKB <akb@3azouz.net>
2019-10-11 12:07:16 +0200

    catching overflow exceptions

--- entropix/filtering/informativeness.py
+++ entropix/filtering/informativeness.py
@@ -31,7 +31,8 @@ class Informativeness():
 
     @lru_cache(maxsize=50)
     def _get_prob_distribution(self, context):
-        word2_indices = [self._model.wv.vocab[w].index for w in context if w in self._model.wv.vocab]
+        word2_indices = [self._model.wv.vocab[w].index for w in context if
+                         w in self._model.wv.vocab]
         l1 = np.sum(self._model.wv.vectors[word2_indices], axis=0)
         if word2_indices and self._model.cbow_mean:
             l1 /= len(word2_indices)
@@ -43,10 +44,14 @@ class Informativeness():
     @lru_cache(maxsize=10)
     def context_informativeness(self, context):
         """Get context informativeness (CI)."""
-        probs = self._get_prob_distribution(context)
-        shannon_entropy = scipy.stats.entropy(probs)
-        ctx_ent = 1 - (shannon_entropy / np.log(len(probs)))
-        return ctx_ent
+        try:
+            probs = self._get_prob_distribution(context)
+            shannon_entropy = scipy.stats.entropy(probs)
+            ctx_ent = 1 - (shannon_entropy / np.log(len(probs)))
+            return ctx_ent
+        except RuntimeWarning:
+            # catch overflow exceptions
+            return 0
 
     @lru_cache(maxsize=10)
     def context_word_informativeness(self, context, word_index):
@@ -58,7 +63,9 @@ class Informativeness():
         ctx_without_word = tuple(x for idx, x in enumerate(context) if
                                  idx != word_index)
         if not ctx_without_word:
-            if ctx_info_with_word > .25:  # arbitrary value set to keep relatively informative words in particular contexts
+            # arbitrary value set to keep relatively informative words in
+            # particular contexts
+            if ctx_info_with_word > .25:
                 return 1
             return -1
         ctx_info_without_word = self.context_informativeness(ctx_without_word)
