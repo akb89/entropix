dd52b174fc6a9b49169d8230fa36bcdd244cac3f
AKB <akb@3azouz.net>
2019-12-10 21:08:30 +0100

    updated XP

--- experiments/xp206.py
+++ experiments/xp206.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     START = 0
     END = 10000
     SCALE = 1e4  # scaling factor for RMSE
-    print('Running entropix XP#203 on MEN-LIMIT-30')
+    print('Running entropix XP#206 on SIMLEX-RAND-30')
     MODEL_NAMES = ['enwiki07', 'oanc', 'enwiki2', 'acl', 'enwiki4', 'bnc']
     com_xp.launch_xp(MODEL_NAMES, SVD_DIRPATH, START, END, SCALE,
                      RESULTS_FILEPATH, randomize=True,
