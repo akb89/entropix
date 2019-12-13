bd4740a7724ebfb6f5003d90c12131ab9233d08d
AKB <akb@3azouz.net>
2019-12-13 16:57:31 +0100

    trying with ndim=1000

--- experiments/xp207.py
+++ experiments/xp207.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     SVD_DIRPATH = '/home/kabbach/entropix/models/frontiers/aligned/'
     RESULTS_FILEPATH = '/home/kabbach/entropix/models/frontiers/results/xp207.results'
     START = 0
-    END = 10000
+    END = 1000
     SCALE = 1e4  # scaling factor for RMSE
     print('Running entropix XP#207 on SVD-FULL-10000')
     MODEL_NAMES = ['enwiki07', 'oanc', 'enwiki2', 'acl', 'enwiki4', 'bnc']
