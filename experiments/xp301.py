8b50c1108a64dedc1ed3aa3e69898a8d3f0a01b3
AKB <akb@3azouz.net>
2019-12-15 20:35:18 +0100

    updated XP

--- experiments/xp301.py
+++ experiments/xp301.py
@@ -13,10 +13,10 @@ if __name__ == '__main__':
     END = 10000
     SCALE = 1e4
     BIN_SIZE = 30
-    # SVD_DIRPATH = '/home/kabbach/entropix/models/frontiers/aligned/'
-    SVD_DIRPATH = '/Users/akb/Github/entropix/models/frontiers/aligned/'
-    # MODEL_NAMES = ['enwiki07', 'oanc', 'enwiki2', 'acl', 'enwiki4', 'bnc']
-    MODEL_NAMES = ['enwiki07', 'oanc']
+    SVD_DIRPATH = '/home/kabbach/entropix/models/frontiers/aligned/'
+    # SVD_DIRPATH = '/Users/akb/Github/entropix/models/frontiers/aligned/'
+    MODEL_NAMES = ['enwiki07', 'oanc', 'enwiki2', 'acl', 'enwiki4', 'bnc']
+    # MODEL_NAMES = ['enwiki07', 'oanc']
     models = com_xp.load_aligned_models(MODEL_NAMES, SVD_DIRPATH, START, END)
     for tuple1, tuple2 in itertools.combinations(models, 2):
         name1 = tuple1[0]
@@ -29,16 +29,16 @@ if __name__ == '__main__':
         z, t, _ = aligner.align_vocab(model1, model2, vocab1, vocab2)
         rmses = []
         xcorrx = []
-        for idx in tqdm(range(model1.shape[1])):
+        for idx in tqdm(range(z.shape[1])):
             if idx % BIN_SIZE == 0:
-                if idx + BIN_SIZE > model1.shape[1]:
+                if idx + BIN_SIZE > z.shape[1]:
                     break
-                m1 = model1[:, idx:idx+BIN_SIZE]
-                m2 = model2[:, idx:idx+BIN_SIZE]
+                m1 = z[:, idx:idx+BIN_SIZE]
+                m2 = t[:, idx:idx+BIN_SIZE]
                 rmses.append(com_xp.get_rmse(m1, m2) * SCALE)
-            xcorr = abs(metrix.pearson_correlation(model1[:, idx], model2[:, idx]))
+            xcorr = abs(metrix.pearson_correlation(z[:, idx], t[:, idx]))
             xcorrx.append(xcorr)
         avgs = com_xp.binize(np.array(xcorrx), BIN_SIZE).mean(axis=1)
-        with open('{}-{}-pearson-rmse.dat'.format(name1, name2), 'w', encoding='utf-8') as outs:
-            for avg, rmse in zip(avgs, xcorrx):
+        with open('{}-{}-pearson-rmse-n{}.dat'.format(name1, name2, BIN_SIZE), 'w', encoding='utf-8') as outs:
+            for avg, rmse in zip(avgs, rmses):
                 print('{}\t{}'.format(avg, rmse), file=outs)
